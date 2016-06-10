94f1d3c1ce07ab3601c11e898b2ad89396de388d
Ben Racher <benjamin.racher@gmail.com>
2016-06-10 13:03:46 -0500

    Backtrace breaks ruby contract and makes tests fails, renaming to stack_trace

--- lib/trackvia-api-sdk/exceptions.rb
+++ lib/trackvia-api-sdk/exceptions.rb
@@ -1,14 +1,14 @@
 module Trackvia
   class ApiError < StandardError
-    attr_reader :cause, :errors, :message, :name, :code, :backtrace, :error, :description
+    attr_reader :cause, :errors, :message, :name, :code, :stack_trace, :error, :description
 
-    def initialize(cause, errors, message, name, code, backtrace, error, description)
+    def initialize(cause, errors, message, name, code, stack_trace, error, description)
       @cause = cause
       @errors = errors
       @message = message
       @name = name
       @code = code
-      @backtrace = backtrace
+      @stack_trace = stack_trace
       @error = error
       @description = description
     end
